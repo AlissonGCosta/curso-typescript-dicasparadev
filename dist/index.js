a1f8d23434f14c7f50312898641b0a105fbab38d
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-30 22:14:43 -0300

    Adiciona variáveis e enum para estruturação de dados no TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let age = 5;
-console.log(age);
+const name = "Felpe";
+const isValid = true;
+let idk = 5;
+idk = "Hello";
+idk = true;
+const ids = [1, 2, 3, 4, 5];
+const booleans = [true, false, true];
+const names = ["Felpe", "John", "Doe"];
+//tuple
+const person = [1, "Felpe", true];
+//lista de tulples
+const people = [
+    [1, "Alisson"],
+    [2, "Felipe"]
+];
+// insterections
+const productID = '2';
+//enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["up"] = 1] = "up";
+    Direction1[Direction1["down"] = 2] = "down";
+})(Direction1 || (Direction1 = {}));
+const direction = Direction1.up;
+console.log(direction);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,39 @@
 let age: number = 5;
+const name: string = "Felpe";
+const isValid: boolean = true;
+let idk: any = 5;
 
+idk = "Hello";
+idk = true;
 
-console.log(age);
+const ids: number[] = [1,2,3,4,5];
+const booleans: boolean[] = [true, false, true];
+const names: string[] = ["Felpe", "John", "Doe"];
+
+//tuple
+
+const person:[number, string, boolean] = [1, "Felpe", true];
+
+//lista de tulples
+
+const people:[number,string][] = [
+
+    [1,"Alisson"],
+    [2,"Felipe"]
+
+]
+
+// insterections
+
+const productID: string | number = '2';
+
+//enum
+
+enum Direction1{
+    up = 1,
+    down = 2
+}
+
+const direction = Direction1.up;
+
+console.log(direction);
